e2edd20fd5ae6932b6e1d9bc0fa52496b7d6c5dc
Tyler Hebenstreit <tyler.hebenstreit@Tyler-Hebenstreits-MacBook-Pro.local>
2018-02-26 13:54:28 -0800

    CDP-490: Updated WP REST response to include srt file URL as a string (srcUrl) in the srt object property of the unit.

--- includes/class-cdp-video-post-type-rest-controller.php
+++ includes/class-cdp-video-post-type-rest-controller.php
@@ -129,7 +129,7 @@ if (is_plugin_active($required_plugin)) {
 
         foreach ($srts as $srt) {
           if ( in_array($key, $srt, true) && isset($srt['_cdp_video_srts_srt_file']) ) {
-            $unit->srt = $srt['_cdp_video_srts_srt_file'];
+            $unit->srt = (object) array('srcUrl' => $srt['_cdp_video_srts_srt_file']);
           }
         }
 
